a6b4320711933c6d4e4a96accdbfd7bef6d78d01
VenGogh <79319566+VenGogh@users.noreply.github.com>
2023-01-08 15:22:13 +0800

    Update cp_file_top.sh

--- cp_file_top.sh
+++ cp_file_top.sh
@@ -1,5 +1,5 @@
 while true
 do 
-cp -a /content/stable-diffusion-webui/outputs/txt2img-images/ /content/drive/computer/mycomputer/Documents/
+cp -a /content/stable-diffusion-webui/outputs/txt2img-images/ /content/drive/computer/mycomputer/
 sleep 1m
 done
